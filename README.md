be44b977261ec8a9ba37763aad5deff5d8997e5d
jcguy <corder.guy@gmail.com>
2019-11-20 17:04:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# avalon
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/avalon)
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA0lDX8TPtcQ4JD2Qc9rM03F3c6E0GiAGQ",
+    authDomain: "corder-avalon.firebaseapp.com",
+    databaseURL: "https://corder-avalon.firebaseio.com",
+    projectId: "corder-avalon",
+    storageBucket: "corder-avalon.appspot.com",
+    messagingSenderId: "1026444721892",
+    appId: "1:1026444721892:web:b127d0d8f59356446c76f1",
+    measurementId: "G-97MLE1GMF2"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
